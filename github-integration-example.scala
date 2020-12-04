a67e3eb08392eb40d2584eab89ab312f6a625b2b
Megan Feichtel <meganfeichtel@gmail.com>
2020-12-04 17:54:36 -0500

    add new example file

---
+++ github-integration-example.scala
@@ -0,0 +1,20 @@
+// Databricks notebook source
+val someDF = Seq(
+  (40, "bat"),
+  (2, "mouse"),
+  (25, "cat"),
+  (5, "dog"),
+  (100, "tiger"),
+  (30, "horse")
+).toDF("number", "word")
+
+display(someDF)
+
+// COMMAND ----------
+
+val filtered = someDF.filter($"number" > 10)
+
+display(filtered)
+
+// COMMAND ----------
+
